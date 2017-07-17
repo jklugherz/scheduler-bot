7e4e3af8c8f1dadf3659d15af5b04d0ff635f210
Nate <n_straus3@outlook.com>
2017-07-17 16:14:22 -0700

    initial setup

--- app.js
+++ app.js
@@ -1,125 +1,38 @@
-var express = require('express');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var LocalStrategy = require('passport-local');
-var mongoose = require('mongoose');
-var connect = process.env.MONGODB_URI;
+var express = require( 'express' );
+var session = require( 'express-session' );
+var path = require( 'path' );
+var bodyParser = require( 'body-parser' );
 
-var REQUIRED_ENV = "SECRET MONGODB_URI".split(" ");
-
-REQUIRED_ENV.forEach(function(el) {
-  if (!process.env[el]){
-    console.error("Missing required env var " + el);
-    process.exit(1);
-  }
-});
-
-
-mongoose.connect(connect);
-
-var models = require('./models');
-
-var routes = require('./routes/routes');
-var auth = require('./routes/auth');
 var app = express();
 
-// view engine setup
-var hbs = require('express-handlebars')({
-  defaultLayout: 'main',
-  extname: '.hbs'
-});
-app.engine('hbs', hbs);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded( { extended: false } ) );
 
-app.use(logger('tiny'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var RtmClient = require( '@slack/client' ).RtmClient;
+var CLIENT_EVENTS = require( '@slack/client' ).CLIENT_EVENTS;
+var RTM_EVENTS = require( '@slack/client' ).RTM_EVENTS;
 
-// Passport
-app.use(session({
-  secret: process.env.SECRET,
-  store: new MongoStore({ mongooseConnection: mongoose.connection })
-}));
+var bot_token = 'xoxb-214873434806-eyESITOHR146yGfFeFYnxNNE';
 
+var rtm = new RtmClient( bot_token );
 
-app.use(passport.initialize());
-app.use(passport.session());
+let channel;
 
-passport.serializeUser(function(user, done) {
-  done(null, user._id);
-});
-
-passport.deserializeUser(function(id, done) {
-  models.User.findById(id, done);
-});
-
-// passport strategy
-passport.use(new LocalStrategy(function(username, password, done) {
-  // Find the user with the given username
-  models.User.findOne({ username: username }, function (err, user) {
-    // if there's an error, finish trying to authenticate (auth failed)
-    if (err) {
-      console.error('Error fetching user in LocalStrategy', err);
-      return done(err);
+// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
+rtm.on( CLIENT_EVENTS.RTM.AUTHENTICATED, ( rtmStartData ) => {
+    for ( const c of rtmStartData.channels ) {
+        if ( c.is_member && c.name === 'general' ) { channel = c.id }
     }
-    // if no user present, auth failed
-    if (!user) {
-      return done(null, false, { message: 'Incorrect username.' });
-    }
-    // if passwords do not match, auth failed
-    if (user.password !== password) {
-      return done(null, false, { message: 'Incorrect password.' });
-    }
-    // auth has has succeeded
-    return done(null, user);
-  });
-}
-));
+    console.log( `Logged in as ${ rtmStartData.self.name } of team ${ rtmStartData.team.name }, but not yet connected to a channel` );
+} );
 
-app.use('/', auth(passport));
-app.use('/', routes);
+// you need to wait for the client to fully connect before you can send messages
+rtm.on( CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
+    rtm.sendMessage( 'Planner King active!', channel );
+} );
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+rtm.on( RTM_EVENTS.MESSAGE, ( message ) => {
 
-// error handlers
+} )
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log('Express started. Listening on port %s', port);
-
-module.exports = app;
+rtm.start();
