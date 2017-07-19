dc255daebbc79e2f1881e157a6ca3fa1f77ae346
Nate <n_straus3@outlook.com>
2017-07-19 16:48:20 -0700

    fix

--- bot.js
+++ bot.js
@@ -103,12 +103,10 @@ rtm.on( RTM_EVENTS.MESSAGE, ( msg ) => {
                             let textArr = tempMsg.split( ' ' );
                             textArr = textArr.map( function ( word ) {
                                 if ( word.includes( '<@' ) ) {
-                                    const axiosRet = axios.post( 'https://slack.com/api/users.profile.get', {
+                                    return axios.post( 'https://slack.com/api/users.profile.get', {
                                         token: process.env.SLACK_BOT_TOKEN,
                                         user: word.slice( 2, word.length - 1 )
-                                    } )
-                                    while (!axiosRet.ok) {}
-                                    return axiosRet.profile.email
+                                    } ).profile.email
                                 } else { return word }
                             } )
                             tempMsg = textArr.join(' ');
