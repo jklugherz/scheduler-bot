4ff4000e894a5e99a7b2db108737af676b1cd36d
Nate <n_straus3@outlook.com>
2017-07-19 17:15:51 -0700

    fix

--- bot.js
+++ bot.js
@@ -103,8 +103,7 @@ rtm.on( RTM_EVENTS.MESSAGE, ( msg ) => {
                             textArr = textArr.map( function ( word ) {
                                 if ( word.includes( '<@' ) ) {
                                     const u = rtm.dataStore.getUserById(word.slice( 2, word.length - 1 ));
-                                    console.log(u);
-                                    return u;
+                                    return u.profile.real_name;
                                 } else { return word }
                             } )
                             tempMsg = textArr.join( ' ' );
